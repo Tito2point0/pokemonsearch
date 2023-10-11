967933925435f3b35d00590ef293eb195bc451b9
sanjiro <defconx00@gmail.com>
2023-10-11 19:57:46 -0400

    ./

--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ const FRONTEND_PORT = 3003
 const INDEX_HTML_PATH = './frontend/index.html'
 const INDEX_JS_PATH = './frontend/index.js'
 const DIST_FOLDER = 'dist'
-const BUNDLE_FILE = '/index.js'
+const BUNDLE_FILE = './index.js'
 const AUDIO = 'audio/'
 
 const SOURCE_MAP = IS_DEV ? 'source-map' : false
