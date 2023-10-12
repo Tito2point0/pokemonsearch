c4255a8996e9a40ea460d76fa9c5694f5813c491
sanjiro <defconx00@gmail.com>
2023-10-11 21:18:44 -0400

    pokesearch updated

--- frontend/components/childComponents/PokeSearchForm.js
+++ frontend/components/childComponents/PokeSearchForm.js
@@ -1,30 +1,34 @@
-import React from 'react';
-import { connect } from 'react-redux';
-import { fetchPokemon } from '../actions/action';
-import { useState } from 'react';
-function PokeSearchForm(props) {
-  const [searchTerm, setSearchTerm ] = useState('')
+import React, { useState } from "react";
+import { connect } from "react-redux";
+import { fetchPokemon } from "../actions/action";
 
+function PokeSearchForm(props) {
+  const [searchTerm, setSearchTerm] = useState("");
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    props.fetchPokemon(searchTerm)
+    props.fetchPokemon(searchTerm);
   };
+
   const handleChange = (e) => {
     setSearchTerm(e.target.value);
-} 
+  };
+
   return (
     <div>
       <form onSubmit={handleSubmit}>
-        <input
-   onChange={handleChange}
-        />
-        <button className='initialButton' type="submit">Search</button>
+        <input onChange={handleChange} />
+        <button className="button" type="submit">
+          <span className="button-text">Search By Name</span>
+          {props.loading && <div className="spinner"></div>}
+        </button>
       </form>
-      
     </div>
   );
 }
 
+const mapStateToProps = (state) => ({
+  loading: state.loading,
+});
 
-export default connect(null,{fetchPokemon})(PokeSearchForm);
+export default connect(mapStateToProps, { fetchPokemon })(PokeSearchForm);
